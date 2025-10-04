7e6b204f512da89f7e8ae77d1ada9d789053f162
Leo Wang <meimprankster@gmail.com>
2025-10-04 16:13:44 +0800

    upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   // replace this with your deployed domain
@@ -39,6 +37,5 @@ export default defineConfig({
     extendDefaultPlugins: true,
   },
 
-  output: "server",
-  adapter: netlify(),
+  output: "static",
 });
